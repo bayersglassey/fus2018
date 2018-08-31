936875a49f8d86be0bbfad4497f866f463ba14ee
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-08-30 20:26:52 -0700

    got rid of test stuff in pathutil.[ch], replaced with fus_path_t

--- src/pathutil.c
+++ src/pathutil.c
@@ -1,101 +1,37 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
-#include "util.h"
+#include "includes.h"
 
 
-#include <unistd.h>
-#include <dirent.h>
-#include <errno.h>
-#include <limits.h>
-
-
-#define MAX_SEARCHES 10
-
-
-int find_file(char *name, int name_len, bool *found_ptr){
-    int err = 0;
-    DIR *dir = opendir(".");
-    if(dir == NULL){
-        ERR_INFO();
-        fprintf(stderr, "Couldn't open current directory.\n");
-        perror(NULL);
-        err = 2; goto err;
-    }
-
-    struct dirent *ent;
-    while((ent = readdir(dir))){
-        int ent_name_len = strlen(ent->d_name);
-        if(ent_name_len == name_len && !strncmp(ent->d_name, name, name_len)){
-            *found_ptr = true;
-            goto err;
-        }
-    }
-
-    *found_ptr = false;
-
-err:
-    if(dir != NULL && closedir(dir)){
-        ERR_INFO();
-        fprintf(stderr, "Couldn't close current directory.\n");
-        perror(NULL);
-        err = 2;
-    }
-    return err;
+void fus_path_cleanup(fus_path_t *path){
+    /* ain't gotta do nuthin */
 }
 
-
-int find_file_parent(char *start_path, char *name, int name_len,
-    char *path, size_t path_size
-){
-    int err;
-
-    if(chdir(start_path)){
-        ERR_INFO();
-        fprintf(stderr, "Couldn't change to start directory: %s\n",
-            start_path);
-        perror(NULL);
-        return 2;
-    }
-
-    int n_searches = 0;
-    while(1){
-        bool found = false;
-        err = find_file(name, name_len, &found);
-        if(err)return err;
-        if(found){
-            if(!getcwd(path, path_size)){
-                ERR_INFO();
-                fprintf(stderr, "Couldn't get cwd\n");
-                perror(NULL);
-                return 2;
-            }
-            return 0;
-        }
-
-        if(n_searches >= MAX_SEARCHES){
-            ERR_INFO();
-            fprintf(stderr,
-                "Not found, max number of chdirs to \"..\" was %i\n",
-                MAX_SEARCHES);
-            return 2;
-        }
-
-        if(chdir("..")){
-            ERR_INFO();
-            fprintf(stderr, "Couldn't change to parent directory\n");
-            perror(NULL);
-            return 2;
-        }
-
-        n_searches++;
-    }
-
-    strncpy(path, "", path_size);
+int fus_path_init(fus_path_t *path){
+    path->path[0] = '\0';
+    path->path_size = FUS_PATH_MAX;
     return 0;
 }
 
+int fus_path_add(fus_path_t *path, const char *part, int part_len){
+    int path_len = strlen(path->path);
+    int new_path_len = path_len + part_len;
+    int max_path_len = path->path_size - 1;
+    if(new_path_len > max_path_len){
+        ERR_INFO();
+        fprintf(stderr,
+            "Path obtained from joining \"%s\" + \"%.*s\" would "
+            "have length %i, but maximum allowed length is %i.\n",
+            path->path, part_len, part,
+            new_path_len, max_path_len);
+        return 2;
+    }
+    strncpy(path->path + path_len, part, part_len);
+    path->path[new_path_len] = '\0';
+    return 0;
+}
+
+int fus_path_add_separator(fus_path_t *path){
+    char part[2] = {FUS_PATH_SEPARATOR, '0'};
+    return fus_path_add(path, part, 1);
+}
 
--- src/pathutil.h
+++ src/pathutil.h
@@ -2,8 +2,22 @@
 #define _FUS_PATHUTIL_H_
 
 
-int find_file_parent(char *start_path, char *name, int name_len,
-    char *path, size_t path_size);
+/* What the hell do I include to get PATH_MAX?
+Whatever, heeere I go agaaaain on my oooowooown */
+#define FUS_PATH_MAX 1024
+
+#define FUS_PATH_SEPARATOR '/'
+
+
+typedef struct fus_path {
+    char path[FUS_PATH_MAX];
+    size_t path_size;
+} fus_path_t;
+
+void fus_path_cleanup(fus_path_t *path);
+int fus_path_init(fus_path_t *path);
+int fus_path_add(fus_path_t *path, const char *part, int part_len);
+int fus_path_add_separator(fus_path_t *path);
 
 
 #endif
