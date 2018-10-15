f5612603abde10f1c39c3802590607949fac077f
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-10-15 15:34:45 -0700

    newvalues: array.c: fus_array_set_len doubles size until >= len

--- src/newvalues/array.c
+++ src/newvalues/array.c
@@ -26,24 +26,29 @@ void fus_array_set_len(fus_array_t *array, fus_array_len_t new_len){
     fus_core_t *core = class->core;
     size_t elem_size = class->instance_size;
 
-    fprintf(stderr, "%s: WARNING: Reallocating array so size == len. "
-        "TODO: move to the usual \"double allocated size until >= len\" "
-        "approach.\n",
-        __func__);
-    size_t new_size = new_len * elem_size;
-    char *new_elems = fus_realloc(core, array->elems, new_size);
+    /* Grow allocated memory as needed.
+    NOTE: We never shrink allocated memory!
+    It would make sense to add an option for this... */
+    size_t new_size_min = new_len * elem_size;
+    size_t new_size = array->size;
+    if(new_size == 0)new_size = new_size_min;
+    while(new_size < new_size_min)new_size *= 2;
+    if(new_size != array->size){
+        char *new_elems = fus_realloc(core, array->elems, new_size);
+        array->elems = new_elems;
+        array->size = new_size;
+    }
 
+    char *elems = array->elems;
     for(int i = array->len; i < new_len; i++){
-        void *elem = new_elems + i * elem_size;
+        void *elem = elems + i * elem_size;
         fus_class_instance_init(class, elem);
     }
     for(int i = array->len - 1; i >= new_len; i--){
-        void *elem = new_elems + i * elem_size;
+        void *elem = elems + i * elem_size;
         fus_class_instance_cleanup(class, elem);
     }
 
-    array->elems = new_elems;
-    array->size = new_size;
     array->len = new_len;
 }
 
