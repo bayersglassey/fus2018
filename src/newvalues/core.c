4ce48d0ea511bac1b17c6ba0be94e74918eb2a8f
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-10-18 08:59:17 -0700

    newvalues: core.[ch]: added fus_strnlen, fus_strndup

--- src/newvalues/core.c
+++ src/newvalues/core.c
@@ -1,5 +1,7 @@
 
 
+#include <string.h>
+
 #include "core.h"
 
 
@@ -73,5 +75,19 @@ void *fus_memcpy(fus_core_t *core, void *ptr, void *srcptr, size_t n){
         fus_exit(core, EXIT_FAILURE);
     }
     return new_ptr;
+}
+
+size_t fus_strnlen(fus_core_t *core, const char *s, size_t maxlen){
+    size_t len = 0;
+    while(len < maxlen && s[len] != '\0')len++;
+    return len;
+}
+
+char *fus_strndup(fus_core_t *core, const char *s1, size_t len){
+    size_t s_len = fus_strnlen(core, s1, len);
+    char *s2 = fus_malloc(core, s_len + 1);
+    strncpy(s2, s1, len);
+    s2[s_len] = '\0';
+    return s2;
 }
 
