06a9dad6b097e32f2c8b57f3ddbbd5b1d6fe58ec
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-10-20 21:14:06 -0700

    newvalues: the lexer tests fail, as they should

--- src/newvalues/lexer.c
+++ src/newvalues/lexer.c
@@ -8,5 +8,5 @@ void fus_lexer_load_chunk(fus_lexer_t *lexer,
     const char *chunk, size_t chunk_len){}
 void fus_lexer_next(fus_lexer_t *lexer){}
 bool fus_lexer_done(fus_lexer_t *lexer){return true;}
-bool fus_lexer_got(fus_lexer_t *lexer, const char *token){return true;}
+bool fus_lexer_got(fus_lexer_t *lexer, const char *token){return false;}
 
