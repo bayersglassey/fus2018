182928e480beeb0f9335671fd47d4394f6e902bf
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-11-05 10:15:10 -0800

    newvalues: fun.[ch]: added fus_value_fun

--- src/newvalues/fun.c
+++ src/newvalues/fun.c
@@ -15,3 +15,12 @@ void fus_fun_cleanup(fus_vm_t *vm, fus_fun_t *f){
     fus_arr_cleanup(vm, &f->data);
 }
 
+
+
+fus_value_t fus_value_fun(fus_vm_t *vm, fus_arr_t *data){
+    /* Creates a new fun value with the given data. */
+    fus_boxed_t *p = fus_malloc(vm->core, sizeof(*p));
+    fus_boxed_init(p, vm, FUS_BOXED_FUN);
+    fus_fun_init_from_arr(vm, &p->data.f, data);
+    return (fus_value_t)p;
+}
--- src/newvalues/fun.h
+++ src/newvalues/fun.h
@@ -16,4 +16,6 @@ void fus_fun_init(fus_vm_t *vm, fus_fun_t *f);
 void fus_fun_init_from_arr(fus_vm_t *vm, fus_fun_t *f, fus_arr_t *data);
 void fus_fun_cleanup(fus_vm_t *vm, fus_fun_t *f);
 
+fus_value_t fus_value_fun(fus_vm_t *vm, fus_arr_t *data);
+
 #endif
